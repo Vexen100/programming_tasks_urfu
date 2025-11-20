a44d852cf788f788d259e07a87802c5435707941
Vexen100 <van_ole@mail.ru>
2025-11-20 15:38:35 +0500

    Update task5_converter.py

--- Практика №2/task5_converter.py
+++ Практика №2/task5_converter.py
@@ -1,7 +1,22 @@
-def to_roman(arab_number): # преобразование арабских цифр в римские (на вход принимает целое число в форме строки)
+def to_roman(arab_number):
+    """
+    Преобразует арабское число в римскую запись.
+    
+    Аргументы:
+        arab_number: строка, содержащая целое число
+    
+    Возвращает:
+        Строку с римским представлением числа
+    
+    Пример:
+        to_roman("14") -> "XIV"
+    """
     arab_number = int(arab_number)
-    roman_dict = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X',  40: 'XL', 50: 'L',
-                  90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
+    roman_dict = {
+        1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X',
+        40: 'XL', 50: 'L', 90: 'XC', 100: 'C',
+        400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
+    }
     result_roman = ''
     for num in reversed(roman_dict.keys()):
         while arab_number >= num:
@@ -9,8 +24,24 @@ def to_roman(arab_number): # преобразование арабских ци�
             arab_number -= num
     return result_roman
 
-def to_arab(roman_number): # преобразование римских цифр в арабские (на вход принимает строку - римское число)
-    arab_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+def to_arab(roman_number):
+    """
+    Преобразует римское число в арабское представление.
+    
+    Аргументы:
+        roman_number: строка с римским числом
+    
+    Возвращает:
+        Целое число — арабское представление
+    
+    Пример:
+        to_arab("XIV") -> 14
+    """
+    arab_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
     result_arab = 0
     prev_char = 0
     for char in reversed(roman_number):
@@ -20,15 +51,27 @@ def to_arab(roman_number): # преобразование римских циф�
         else:
             result_arab += current_char
         prev_char = current_char
-
     return result_arab
 
-operation = input('Выберите операцию (1 - из римских в арабские, 2 - из арабских в римские): ')
+
+# Сбор данных от пользователя
+operation = input(
+    'Выберите операцию (1 - из римских в арабские, '
+    '2 - из арабских в римские): '
+)
 while operation not in ('1', '2'):
-    operation = input('Неверно! Выберите операцию (1 - из римских в арабские, 2 - из арабских в римские): ')
+    operation = input(
+        'Неверно! Выберите операцию (1 - из римских в арабские, '
+        '2 - из арабских в римские): '
+    )
 
-data_list = [elem.strip() for elem in input('Введите список чисел через запятую (если число одно, то просто введите его): ').split(',')]
+data_input = input(
+    'Введите список чисел через запятую '
+    '(если число одно, то просто введите его): '
+)
+data_list = [elem.strip() for elem in data_input.split(',')]
 
+# Обработка и вывод результата
 result = []
 if operation == '1':
     for number in data_list:
@@ -38,4 +81,3 @@ else:
         result.append(to_roman(number))
 
 print(result)
-
