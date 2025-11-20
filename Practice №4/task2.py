6ddc26f70ca59fbcfe5037416a9d2931c1844483
Vexen100 <van_ole@mail.ru>
2025-11-20 18:19:10 +0500

    Update task2.py

--- Practice №4/task2.py
+++ Practice №4/task2.py
@@ -1,17 +1,62 @@
 class Book:
+    """
+    Базовый класс для представления книги.
+    
+    Атрибуты:
+        title (str): Название книги
+        author (str): Автор книги
+        year (int): Год издания книги
+    """
+
     def __init__(self, title, author, year):
+        """
+        Инициализирует объект книги с указанными параметрами.
+        
+        Параметры:
+            title: Название книги
+            author: Имя автора
+            year: Год издания (целое число)
+        """
         self.title = title
         self.author = author
         self.year = year
 
     def info(self):
+        """
+        Формирует строку с информацией о книге.
+        
+        Возвращает:
+            str: Строка в формате "Автор: [автор], Название: [название], Год: [год]"
+        """
         return f'Автор: {self.author}, Название: {self.title}, Год: {self.year}'
 
 
 class Ebook(Book):
+    """
+    Дочерний класс для представления электронной книги.
+    
+    Наследует от Book и добавляет атрибут формата файла.
+    
+    Атрибуты:
+        format (str): Формат электронного файла (например, PDF, EPUB)
+    """
+
     def __init__(self, title, author, year, format):
+        """
+        Инициализирует объект электронной книги.
+        
+        Параметры:
+            format: Формат электронного файла
+        """
         super().__init__(title, author, year)
         self.format = format
 
     def info(self):
-        return f'Автор: {self.author}, Название: {self.title}, Год: {self.year}, Формат: {self.format}'
+        """
+        Расширяет базовое описание, добавляя информацию о формате.
+        
+        Возвращает:
+            str: Строка с дополнительной информацией о формате файла
+        """
+        base_info = super().info()
+        return f"{base_info}, Формат: {self.format}"
