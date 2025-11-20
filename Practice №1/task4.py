ddac888a9bf437ae9180bd411b373db25896dc8e
Vexen100 <van_ole@mail.ru>
2025-11-20 16:53:03 +0500

    Update task4.py

--- Practice №1/task4.py
+++ Practice №1/task4.py
@@ -1,1 +1,10 @@
-
+text = input().lower()
+char_count = {}
+for char in text:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+sorted_chars = sorted(char_count.items(), key=lambda x: x[1], reverse=True)
+for i in range(3):
+    print(sorted_chars[i][0])
