d1c995c677292a070d85d65b2f91330b0ad39011
Vexen100 <van_ole@mail.ru>
2025-11-20 16:30:41 +0500

    Update task3.py

--- Practice №1/task3.py
+++ Practice №1/task3.py
@@ -1,1 +1,10 @@
+import random
+import string
 
+# Генерация пароля длины n из символов chars
+def generate(chars, n):
+    return ''.join([random.choice(chars) for i in range(n)])
+
+password = generate(string.ascii_uppercase, 3) + generate(string.digits, 3) + generate('!@#$%^&*', 2)
+
+print(password)
