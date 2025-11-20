cd005f3231166c4430617515daf5acf390d455d5
Vexen100 <van_ole@mail.ru>
2025-11-20 16:09:31 +0500

    Update task1.py

--- Practice №1/task1.py
+++ Practice №1/task1.py
@@ -1,1 +1,8 @@
 
+celsius = float(input("Введите температуру в цельсиях: "))
+
+fahrenheit = round((celsius * 9/5) + 32, 2)
+kelvin = round(celsius + 273.15, 2)
+
+print(f'{celsius} C = {fahrenheit} F')
+print(f'{celsius} C = {kelvin} K')
